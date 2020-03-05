da7e7748ef6b703b161fc429eeccd7921ecf7abf
ola <kveola13@student.westerdals.no>
2020-03-05 22:42:17 +0100

    Added files to learn how to make use of Lodash, Webpack and Yarn.

---
+++ Lodash/mathFunc.js
@@ -0,0 +1,17 @@
+import _ from 'lodash';
+
+export function calculateMin(arr) {
+    return _.min(arr);
+}
+
+export function calculateMax(arr) {
+    return _.max(arr);
+}
+
+export function calculateMean(arr) {
+    return _.mean(arr);
+}
+
+export function computeSum(arr) {
+    return _.sum(arr);
+}

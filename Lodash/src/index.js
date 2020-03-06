abe165260d28eaa380ebcf3f7db93750953964ba
ola <kveola13@student.westerdals.no>
2020-03-06 21:21:03 +0100

    Moved files to suit webpack

--- Lodash/src/index.js
+++ Lodash/src/index.js
@@ -3,15 +3,15 @@ import _ from 'lodash'
 import {calculateMax, calculateMean, calculateMin, computeSum} from "./mathFunc.js";
 
 function initMath() {
-    const arr = [4,5,3,6,8,8,4,4,1];
+    const arr = [4, 5, 3, 6, 8, 8, 4, 4, 1];
 
     const sum = computeSum(arr);
     const mean = calculateMean(arr);
     const max = calculateMax(arr);
     const min = calculateMin(arr);
-
+    console.log(sum);
     document.getElementById("arrayStringId").innerHTML = "[" + _.join(arr, ", ") + "]";
-    document.getElementById("sumId").innerHTML = sum;
+    document.getElementById("sumId").innerText = sum;
     document.getElementById("meanId").innerHTML = mean;
     document.getElementById("maxId").innerHTML = max;
     document.getElementById("minId").innerHTML = min;
